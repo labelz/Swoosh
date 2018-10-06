240adac5483ac2efecc9ac464efabe6d8d7a8a38
Achira Trakansutleart <achirat@bluesharp.co.th>
2018-10-06 23:13:28 +0700

    add desired page

---
+++ app/src/main/java/th/co/bluesharp/swoosh/DesiredActivity.kt
@@ -0,0 +1,12 @@
+package th.co.bluesharp.swoosh
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DesiredActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_desired)
+    }
+}
