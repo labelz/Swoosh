11cbe3685b8cef88224ca47c6d4c262aca8cc64f
Achira Trakansutleart <achirat@bluesharp.co.th>
2018-10-07 16:47:48 +0700

    intent

--- app/src/main/java/th/co/bluesharp/swoosh/WelcomeActivity.kt
+++ app/src/main/java/th/co/bluesharp/swoosh/WelcomeActivity.kt
@@ -1,12 +1,18 @@
 package th.co.bluesharp.swoosh
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        startBtn.setOnClickListener {
+            val leagueIntent = Intent(this, DesiredActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
